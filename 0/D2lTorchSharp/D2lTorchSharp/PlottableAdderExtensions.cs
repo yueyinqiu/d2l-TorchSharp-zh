34338ad43f84404cd10f2738bb414ad4a9996158
yueyinqiu <yueyinqiu@outlook.com>
2024-04-24 22:24:32 +0800

    2.6

--- 0/D2lTorchSharp/D2lTorchSharp/PlottableAdderExtensions.cs
+++ 0/D2lTorchSharp/D2lTorchSharp/PlottableAdderExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using ScottPlot;
 using ScottPlot.Plottables;
+using TorchSharp;
 using static TorchSharp.torch;
 
 namespace Yueyinqiu.Su.D2lTorchSharp;
@@ -7,24 +8,28 @@ namespace Yueyinqiu.Su.D2lTorchSharp;
 public static class PlottableAdderExtensions
 {
     public static Scatter ScatterLine(this PlottableAdder adder, 
-        Tensor xs, Tensor ys,
+        Tensor? xs, Tensor ys,
         string legend = "",
         LinePattern pattern = LinePattern.Solid,
         Color? color = null)
     {
-        if (xs.Dimensions is not 1)
-            throw new ArgumentException(
-                $"{nameof(xs)}.{nameof(xs.Dimensions)} should be equals to one.",
-                nameof(xs));
         if (ys.Dimensions is not 1)
             throw new ArgumentException(
                 $"{nameof(ys)}.{nameof(ys.Dimensions)} should be equals to one.",
                 nameof(ys));
 
+        if (xs is not null && xs.Dimensions is not 1)
+            throw new ArgumentException(
+                $"{nameof(xs)}.{nameof(xs.Dimensions)} should be equals to one.",
+                nameof(xs));
+
         using (NewDisposeScope())
         {
-            xs = xs.detach().cpu().@double();
             ys = ys.detach().cpu().@double();
+            if (xs is null)
+                xs = arange(0, ys.size(0), ScalarType.Float64, device("cpu"), false);
+            else
+                xs = xs.detach().cpu().@double();
 
             var xArray = xs.data<double>().ToArray();
             var yArray = ys.data<double>().ToArray();
