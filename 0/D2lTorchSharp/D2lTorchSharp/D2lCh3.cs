2ae7e3f398cb7e1dd49b233d492a75a65e395ef0
yueyinqiu <yueyinqiu@outlook.com>
2024-05-06 19:48:29 +0800

    d

--- 0/D2lTorchSharp/D2lTorchSharp/D2lCh3.cs
+++ 0/D2lTorchSharp/D2lTorchSharp/D2lCh3.cs
@@ -1,4 +1,6 @@
-﻿using static TorchSharp.torch;
+﻿using TorchSharp;
+using TorchSharp.Modules;
+using static TorchSharp.torch;
 
 namespace Yueyinqiu.Su.D2lTorchSharp;
 
@@ -16,6 +18,46 @@ partial class D2l
                 return (X.MoveToOuterDisposeScope(),
                     y.reshape([-1, 1]).MoveToOuterDisposeScope());
             }
+        }
+
+        public static (DataLoader<Dictionary<string, Tensor>, (Tensor data, Tensor label)> trainIter,
+            DataLoader<Dictionary<string, Tensor>, (Tensor data, Tensor label)> testIter)
+            load_data_fashion_mnist(int batch_size, torchvision.ITransform transform)
+        {
+            static (Tensor data, Tensor label) Collate(
+                IEnumerable<Dictionary<string, Tensor>> d, Device device)
+            {
+                using (NewDisposeScope())
+                {
+                    var data = cat(d.Select((item) => item["data"]).ToArray());
+                    var label = cat(d.Select((item) => item["label"].unsqueeze(0)).ToArray());
+                    return (data.to(device).MoveToOuterDisposeScope(),
+                        label.to(device).MoveToOuterDisposeScope());
+                }
+            }
+
+            var train = torchvision.datasets.FashionMNIST(
+                "../gitignored/data", train: true, download: true, transform);
+            var test = torchvision.datasets.FashionMNIST(
+                "../gitignored/data", train: false, download: true, transform);
+            return (new(train, batch_size, Collate, shuffle: true, num_worker: 4), 
+                new(train, batch_size, Collate, shuffle: false, num_worker: 4));
+        }
+
+        public static (DataLoader<Dictionary<string, Tensor>, (Tensor data, Tensor label)> trainIter,
+            DataLoader<Dictionary<string, Tensor>, (Tensor data, Tensor label)> testIter)
+            load_data_fashion_mnist(int batch_size, (int height, int width) resize)
+        {
+            var transform = torchvision.transforms.Resize(resize.height, resize.width);
+            return load_data_fashion_mnist(batch_size, transform);
+        }
+
+        public static (DataLoader<Dictionary<string, Tensor>, (Tensor data, Tensor label)> trainIter,
+            DataLoader<Dictionary<string, Tensor>, (Tensor data, Tensor label)> testIter)
+            load_data_fashion_mnist(int batch_size, int resize)
+        {
+            var transform = torchvision.transforms.Resize(resize);
+            return load_data_fashion_mnist(batch_size, transform);
         }
     }
 }
---
+++ 0/D2lTorchSharp/D2lTorchSharp/FashionMnistDataLoader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TorchSharp;
+using TorchSharp.Modules;
+using static TorchSharp.torch;
+using static TorchSharp.torch.utils.data;
+
+namespace Yueyinqiu.Su.D2lTorchSharp;
+public sealed class FashionMnistDataLoader :
+    DataLoader<Dictionary<string, Tensor>, (Tensor data, Tensor label)>
+{
+    internal FashionMnistDataLoader(
+        Dataset dataset, int batchSize, bool shuffle, int numWorker) :
+        base(dataset, batchSize, Collate, shuffle: shuffle, num_worker: numWorker)
+    {
+    }
+
+    private static (Tensor data, Tensor label) Collate(
+        IEnumerable<Dictionary<string, Tensor>> d, Device device)
+    {
+        using (NewDisposeScope())
+        {
+            var data = cat(d.Select((item) => item["data"]).ToArray());
+            var label = cat(d.Select((item) => item["label"].unsqueeze(0)).ToArray());
+            return (data.to(device).MoveToOuterDisposeScope(),
+                label.to(device).MoveToOuterDisposeScope());
+        }
+    }
+}
