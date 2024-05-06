8a695b467a42f0cfed0b779d32d2563fde78cc0c
yueyinqiu <yueyinqiu@outlook.com>
2024-05-06 18:43:22 +0800

    3.5

---
+++ 0/D2lTorchSharp/D2lTorchSharp/EnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using ScottPlot;
+using ScottPlot.DataSources;
+using ScottPlot.Plottables;
+using System.Reactive.Joins;
+using static TorchSharp.torch;
+
+namespace Yueyinqiu.Su.D2lTorchSharp;
+
+public static class EnumerableExtensions
+{
+    public static string ToArrayString<T>(this IEnumerable<T> values)
+    {
+        return $"[{string.Join(", ", values)}]";
+    }
+}
